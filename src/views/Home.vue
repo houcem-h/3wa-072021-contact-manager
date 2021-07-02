21e8deb1b7948ba2151fa013cf3f23fe41776d7d
Houcem Hedhly <hedhoucem@gmail.com>
2021-07-02 15:37:49 +0100

    creating data source

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,48 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Vue.js workshop" />
-  </div>
+  <div class="home"></div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
+  components: {},
+  data() {
+    return {
+      contactsList: [
+        {
+          id: 1,
+          name: "Leanne Graham",
+          email: "leanne.graham@gmail.com",
+          phone: "+21622334455",
+          address: {
+            street: "Kulas Light",
+            city: "Gwenborough",
+          },
+        },
+        {
+          id: 2,
+          name: "Ervin Howell",
+          email: "Shanna@melissa.tv",
+          phone: "010-692-6593 x09125",
+          address: {
+            street: "Victor Plains",
+            city: "Wisokyburgh",
+          },
+        },
+        {
+          id: 3,
+          name: "Clementine Bauch",
+          email: "Nathan@yesenia.net",
+          phone: "1-463-123-4447",
+          address: {
+            street: "Douglas Extension",
+            city: "McKenziehaven",
+          },
+        },
+      ],
+    };
   },
 };
 </script>
