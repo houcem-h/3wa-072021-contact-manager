5cbc908fa8f889f14f24c911cafb03a950e640c2
Houcem Hedhly <hedhoucem@gmail.com>
2021-07-02 15:20:13 +0100

    more settings

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
+import Contact from '../views/Contact.vue'
 
 Vue.use(VueRouter)
 
@@ -15,6 +16,11 @@ const routes = [
     path: '/about',
     name: 'About',
     component: About
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    component: Contact
   }
 ]
 
---
+++ src/views/Contact.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="contact">
+    <h1>This is the contact page</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
