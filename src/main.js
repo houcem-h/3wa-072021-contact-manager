c113cddad8d855da30fa113994265e92bd93f27a
Houcem Hedhly <hedhoucem@gmail.com>
2021-07-02 15:19:44 +0100

    set dependencies in main.js file

--- src/main.js
+++ src/main.js
@@ -2,7 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+Vue.component('fa-icon', FontAwesomeIcon)
+library.add()
+
 Vue.config.productionTip = false
+
 
 new Vue({
   router,
